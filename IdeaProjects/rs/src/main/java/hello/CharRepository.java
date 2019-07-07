0a12ba080be8f3722a029d730b3e140eed3a0658
Yuri <yyesipov@gmail.com>
2019-07-07 06:12:33 +0300

    commit

--- IdeaProjects/rs/src/main/java/hello/CharRepository.java
+++ IdeaProjects/rs/src/main/java/hello/CharRepository.java
@@ -1,8 +1,24 @@
-/*
 package hello;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CharRepository extends CrudRepository <Character, Integer>{
+
+
+    /*@Query(value = "SELECT * FROM Character c WHERE c.name  LIKE  '% ? %' ", nativeQuery = true)
+    List<Character> findCharByKeyword(String keyword);*/
+
+    @Query(value = "SELECT * FROM Character c WHERE charindex(' ? ', name)> 0", nativeQuery = true)
+    List<Character> findCharByKeyword(String keyword);
+
+
+  @Query ( value = "SELECT * FROM Character c  ORDER BY random() LIMIT 1", nativeQuery = true)
+  Character getRandomChar();
 }
-*/
+
+
+
+
--- IdeaProjects/rs/src/main/java/hello/MainController.java
+++ IdeaProjects/rs/src/main/java/hello/MainController.java
@@ -1,25 +1,27 @@
-/*
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 
 @Controller
-@RequestMapping(path = "/demo")
+@RequestMapping(path="/demo")
 public class MainController {
+
 
     @Autowired
     private CharRepository charRepository;
 
-    @GetMapping(path="/add")
+    @GetMapping(path = "/add")
     public @ResponseBody String addNewCharacter (
             @RequestParam String name, @RequestParam String status,
             @RequestParam String species, @RequestParam String gender,
-            @RequestParam String image, @RequestParam String url){
+            @RequestParam String image, @RequestParam String url) throws Exception{
         Character character = new Character();
         character.setName(name);
         character.setStatus(status);
@@ -32,9 +34,22 @@ public class MainController {
     }
 
     @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Character> getAllCharacters(){
-        return  charRepository.findAll();
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public @ResponseBody
+    Iterable<Character> getAllCharacters(){
+        return charRepository.findAll();
+    }
+
+    @GetMapping(path = "/{keyword}")
+    public @ResponseBody
+    Iterable<Character> getCharByKeyword(@PathVariable("keyword") String keyword){
+        return charRepository.findCharByKeyword(keyword);
+    }
+
+    @GetMapping(path = "/rnd")
+    public @ResponseBody
+   Character getRandomChar(){
+        return charRepository.getRandomChar();
     }
 
 }
-*/
